ac0db20377e2f12b97af55df631566786281b59e
Sone <sthaya@gmail.com>
2020-06-21 15:14:50 -0700

    adjusted knex file directory

--- knexfile.js
+++ knexfile.js
@@ -31,10 +31,10 @@ module.exports = {
       max: 10,
     },
     migrations: {
-      directory: "./database/migrations",
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: "./database/seeds",
+      directory: "./data/seeds",
     },
   },
 };
